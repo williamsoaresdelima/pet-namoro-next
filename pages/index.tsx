367c389304890f07469ae6083d515b07f34e4d6b
wlima <wlima@82Y34G3>
2022-11-29 19:05:30 -0300

    bug fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { GetServerSideProps } from "next";
 
-import IPost from "../src/components/Post/IPost";
 import ProfileHeader from "../src/components/ProfileHeader/ProfileHeader";
 
 const data = {
@@ -15,8 +14,8 @@ const data = {
 
 const token = "eb4402f8fa7a31cf2f2f3e05e0513957a9956e0382486dea845845f5bcdd9be7cdc426c210b347c93e39d51f6877432cf7be76875588d6b04a7165fd1b2d210a00675c23347605d8b1ef867f4e9f9956a72c9c5a50363fd4e425cf90b864242396fc85c04d11757756ab15073863fe3ac02b2ae740531b208c8caa097f340d93"
 
-export default function Home (props : any) {
-  console.log(props)
+// export default function Home (props : any) {
+export default function Home () {
   return (
   <>
     <ProfileHeader {...data}/>
@@ -24,20 +23,15 @@ export default function Home (props : any) {
   )
 }
 
-// const config = {
-//   headers: {
-//     Authorization: `bearer ${token}`
+// axios.defaults.headers.get['Authorization'] = `Bearer ${token}`;
+// export const getServerSideProps : GetServerSideProps = async () => {
+//   const data = await axios.get("http://localhost:1337/api/posts")
+
+//   const posts = await data.data
+
+//   return {
+//     props: {
+//       posts
+//     }
 //   }
 // }
-axios.defaults.headers.get['Authorization'] = `Bearer ${token}`;
-export const getServerSideProps : GetServerSideProps = async () => {
-  const data = await axios.get("http://localhost:1337/api/posts")
-
-  const posts = await data.data
-
-  return {
-    props: {
-      posts
-    }
-  }
-}
