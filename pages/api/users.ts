bd02695097f4d81a837e6cae2f8e6e35056f73de
wlima <wlima@82Y34G3>
2022-11-29 19:01:30 -0300

    implement orm

---
+++ pages/api/users.ts
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export default async function users (req: NextApiRequest, res: NextApiResponse) {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+}
--- src/components/Post/IPost.tsx
+++ src/components/Post/IPost.tsx
@@ -1,5 +1,3 @@
-
-
 export default interface IPost {
   authorImage: string,
   title: string,
